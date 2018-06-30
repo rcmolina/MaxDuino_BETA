f134f68ed4a1cb212d4a25ee82bf8feff50692b9
rcmolina <rcmolina@gmail.com>
2018-06-30 10:09:08 +0200

    Add files via upload

--- MaxProcessing.ino
+++ MaxProcessing.ino
@@ -1004,7 +1004,7 @@ void TZXProcess() {
             lcd.print(PlayBytes);
             lcd.setCursor(0,1);
             //lcd.print(String(bytesRead,HEX) + " - L: " + String(loopCount, DEC));
-            utoa(bytesRead,PlayBytes,16);
+            ltoa(bytesRead,PlayBytes,16);
             lcd.print(PlayBytes) ;  lcd.print(" - L: "); lcd.print(loopCount);
           #endif
 
@@ -1019,9 +1019,9 @@ void TZXProcess() {
                   u8g.print(String(bytesRead,HEX) + " - L: " + String(loopCount, DEC));
               } while( u8g.nextPage() ); */
               printtextF(PSTR("ID? "),0);
-              itoa(currentID,PlayBytes,16);sendStrXY(PlayBytes,4,0);
-              itoa(bytesRead,PlayBytes,16);strcat_P(PlayBytes,PSTR(" - L: "));printtext(PlayBytes,lineaxy);
-              itoa(loopCount,PlayBytes,10);sendStrXY(PlayBytes,10,lineaxy);
+              utoa(currentID,PlayBytes,16);sendStrXY(PlayBytes,4,0);
+              ltoa(bytesRead,PlayBytes,16);strcat_P(PlayBytes,PSTR(" - L: "));printtext(PlayBytes,lineaxy);
+              utoa(loopCount,PlayBytes,10);sendStrXY(PlayBytes,10,lineaxy);
 
           #endif 
           
@@ -1031,10 +1031,12 @@ void TZXProcess() {
             lcd.print("ID? ");
             lcd.setCursor(4,0);
             //lcd.print(String(currentID, HEX));
-            lcd.print(currentID, HEX);
+            utoa(currentID,PlayBytes,16);
+            lcd.print(PlayBytes);
             lcd.setCursor(0,1);
             //lcd.print(String(bytesRead,HEX) + " - L: " + String(loopCount, DEC));
-            lcd.print(bytesRead,HEX) ; // lcd.print(" - L: "); lcd.print(loopCount);
+            ltoa(bytesRead,PlayBytes,16);
+            lcd.print(PlayBytes) ;  lcd.print(" - L: "); lcd.print(loopCount);
           #endif
   
           //delay(8000);
