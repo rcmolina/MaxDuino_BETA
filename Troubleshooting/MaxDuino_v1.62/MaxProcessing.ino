1b0e138929b8d748b3fd9c542b65496076fe5298
rcmolina <rcmolina@gmail.com>
2021-04-24 18:37:14 +0200

    zx80 .o patched

--- Troubleshooting/MaxDuino_v1.62/MaxProcessing.ino
+++ Troubleshooting/MaxDuino_v1.62/MaxProcessing.ino
@@ -1213,7 +1213,7 @@ void TZXProcess() {
             case PAUSE:
               currentPeriod = PAUSELENGTH;
               bitSet(currentPeriod, 15);
-              currentBlockTask=PILOT;
+              currentBlockTask=DATA;
             break; 
             
             case DATA:
