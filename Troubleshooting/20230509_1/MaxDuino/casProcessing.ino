e8d73cbe586fdec7fb9799718e2e7ef2c2e35a18
Rafa Molina <rcmolina@gmail.com>
2023-05-27 22:57:17 +0200

    Add files via upload

--- Troubleshooting/20230509_1/MaxDuino/casProcessing.ino
+++ Troubleshooting/20230509_1/MaxDuino/casProcessing.ino
@@ -114,7 +114,7 @@ void process()
   }
   if(currentTask==TASK::GETFILEHEADER || currentTask==TASK::CAS_wData)
   {
-    if((readfile(8,bytesRead))==8) 
+    if(readfile(8,bytesRead))
     {
       if(!memcmp_P(filebuffer, HEADER,8)) {
         if(fileStage==0) 
